480e16d2d1cfb16c473e62773db1a30bf07ada35
Guillaume Nodet <gnodet@gmail.com>
2022-02-21 10:23:11 +0100

    Simplify the CachingWriter to avoid decoding the existing file

--- plexus-component-metadata/src/main/java/org/codehaus/plexus/metadata/CachingWriter.java
+++ plexus-component-metadata/src/main/java/org/codehaus/plexus/metadata/CachingWriter.java
@@ -1,10 +1,30 @@
 package org.codehaus.plexus.metadata;
 
+/*
+ * Copyright (c) 2022, Codehaus.org
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 import java.io.IOException;
 import java.io.StringWriter;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
 import java.util.Objects;
 
 public class CachingWriter extends StringWriter
@@ -21,27 +41,16 @@ public class CachingWriter extends StringWriter
     @Override
     public void close() throws IOException
     {
-        String str = getBuffer().toString();
-        if ( Files.exists( path ) )
+        byte[] data = getBuffer().toString().getBytes( charset );
+        if ( Files.exists( path ) && Files.size( path ) == data.length )
         {
-            String old = readString( path, charset );
-            if ( str.equals( old ) )
+            byte[] ba = Files.readAllBytes( path );
+            if ( Arrays.equals( data, ba ) )
             {
                 return;
             }
         }
-        writeString( path, str, charset );
+        Files.write( path, data );
     }
 
-    private static String readString( Path path, Charset charset ) throws IOException
-    {
-        byte[] ba = Files.readAllBytes( path );
-        return new String( ba, charset );
-    }
-
-    private static void writeString( Path path, String str, Charset charset ) throws IOException
-    {
-        byte[] ba = str.getBytes( charset );
-        Files.write( path, ba );
-    }
 }
