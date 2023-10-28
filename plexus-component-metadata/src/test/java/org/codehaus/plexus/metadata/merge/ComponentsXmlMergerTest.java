ba2d484a4a9e74e9896b9a9ce72b077976af555a
Sylwester Lachiewicz <slachiewicz@gmail.com>
2023-10-28 18:02:56 +0200

    Switch to JUnit5

--- plexus-component-metadata/src/test/java/org/codehaus/plexus/metadata/merge/ComponentsXmlMergerTest.java
+++ plexus-component-metadata/src/test/java/org/codehaus/plexus/metadata/merge/ComponentsXmlMergerTest.java
@@ -24,43 +24,60 @@ package org.codehaus.plexus.metadata.merge;
  * SOFTWARE.
  */
 
+import javax.inject.Inject;
+
 import java.io.File;
 import java.io.StringReader;
 import java.util.List;
 
-import org.codehaus.plexus.PlexusTestCase;
+import org.codehaus.plexus.PlexusContainer;
 import org.codehaus.plexus.metadata.merge.support.AbstractMergeableElement;
 import org.codehaus.plexus.metadata.merge.support.AbstractMergeableElementList;
 import org.codehaus.plexus.metadata.merge.support.ComponentElement;
 import org.codehaus.plexus.metadata.merge.support.ComponentsElement;
 import org.codehaus.plexus.metadata.merge.support.RequirementsElement;
+import org.codehaus.plexus.testing.PlexusTest;
 import org.codehaus.plexus.util.FileUtils;
 import org.jdom2.Document;
 import org.jdom2.Element;
 import org.jdom2.input.SAXBuilder;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 /**
  * Tests for {@link ComponentsXmlMerger}.
  *
  * @author <a href='mailto:rahul.thakur.xdev@gmail.com'>Rahul Thakur</a>
  */
-public class ComponentsXmlMergerTest extends PlexusTestCase {
-    public void testBasic() throws Exception {
-        ComponentsXmlMerger merger = (ComponentsXmlMerger) lookup(Merger.class, "componentsXml");
+@PlexusTest
+class ComponentsXmlMergerTest {
+
+    @Inject
+    PlexusContainer container;
+
+    @Test
+    void testBasic() throws Exception {
+        ComponentsXmlMerger merger = (ComponentsXmlMerger) container.lookup(Merger.class, "componentsXml");
         assertNotNull(merger);
     }
 
-    public void testComponentsXmlFileMerge() throws Exception {
-        File dominantXml = getTestFile("src/test/resources/org/codehaus/plexus/metadata/merge/dominant.xml");
-        File recessiveXml = getTestFile("src/test/resources/org/codehaus/plexus/metadata/merge/recessive.xml");
+    @Test
+    void testComponentsXmlFileMerge() throws Exception {
+        File dominantXml = new File("src/test/resources/org/codehaus/plexus/metadata/merge/dominant.xml");
+        File recessiveXml = new File("src/test/resources/org/codehaus/plexus/metadata/merge/recessive.xml");
         Document dDoc = new SAXBuilder().build(dominantXml);
         Document rDoc = new SAXBuilder().build(recessiveXml);
         // ComponentsXmlMerger merger = new ComponentsXmlMerger (dDoc);
-        Merger merger = lookup(Merger.class, "componentsXml");
+        Merger merger = container.lookup(Merger.class, "componentsXml");
         assertNotNull(merger);
         merger.merge(dDoc, rDoc);
 
-        File merged_xml = getTestFile("target/merged.xml");
+        File merged_xml = new File("target/merged.xml");
         if (merged_xml.exists()) {
             FileUtils.forceDelete(merged_xml);
         }
@@ -69,26 +86,20 @@ public class ComponentsXmlMergerTest extends PlexusTestCase {
         // read merged xml and verify it was merged as expected
         Document mDoc = new SAXBuilder().build(merged_xml);
         Element mRootElt = mDoc.getRootElement();
-        assertTrue(mRootElt.getName().equals("component-set"));
+        assertEquals("component-set", mRootElt.getName());
         assertEquals(1, mRootElt.getChildren("components").size());
-        List componentEltList = mRootElt.getChild("components").getChildren("component");
+        List<Element> componentEltList = mRootElt.getChild("components").getChildren("component");
         assertEquals(2, componentEltList.size());
-        Element cElt = (Element) componentEltList.get(0);
+        Element cElt = componentEltList.get(0);
 
         assertEquals("org.codehaus.plexus.metadata.component.IComponent", cElt.getChildTextTrim("role"));
         assertEquals(
                 "org.codehaus.plexus.metadata.component.DominantComponent", cElt.getChildTextTrim("implementation"));
 
-        assertEquals(
-                "Should only have 1 description element.",
-                1,
-                cElt.getChildren("description").size());
+        assertEquals(1, cElt.getChildren("description").size(), "Should only have 1 description element.");
         assertEquals("Description for Dominant component", cElt.getChildTextTrim("description"));
 
-        assertEquals(
-                "Should only have 1 configuration element.",
-                1,
-                cElt.getChildren("configuration").size());
+        assertEquals(1, cElt.getChildren("configuration").size(), "Should only have 1 configuration element.");
         // assert Merged configuration properties
         Element configurationElt = cElt.getChild("configuration");
         assertNotNull(configurationElt);
@@ -109,7 +120,8 @@ public class ComponentsXmlMergerTest extends PlexusTestCase {
                 mRootElt.getChild("lifecycle-handler-manager").getAttributeValue("implementation"));
     }
 
-    public void testInvalidMergeableElements() throws Exception {
+    @Test
+    void testInvalidMergeableElements() throws Exception {
         // dominant Component Element
         AbstractMergeableElement dCE = new ComponentElement(new Element("component"));
         Element roleElt = new Element("role");
@@ -131,7 +143,8 @@ public class ComponentsXmlMergerTest extends PlexusTestCase {
      *
      * @throws Exception if there was an unexpected error.
      */
-    public void testComponentsMerge() throws Exception {
+    @Test
+    void testComponentsMerge() throws Exception {
         // dominant Components Element
         AbstractMergeableElement dParent = new ComponentsElement(new Element("components"));
         Element dCE = new Element("component");
@@ -201,12 +214,9 @@ public class ComponentsXmlMergerTest extends PlexusTestCase {
      *
      * @throws Exception in case of an error.
      */
-    public void testDeepComponentsMerge() throws Exception {
-        // FIXME: Review this after MergeStrategies are in place.
-        if (true) {
-            return;
-        }
-
+    @Test
+    @Disabled("FIXME: Review this after MergeStrategies are in place.")
+    void testDeepComponentsMerge() throws Exception {
         // dominant Component Element
         AbstractMergeableElement dCE = new ComponentElement(new Element("component"));
         Element roleElt = new Element("role");
@@ -256,23 +266,21 @@ public class ComponentsXmlMergerTest extends PlexusTestCase {
         dCE.merge(rCE);
 
         // verify the merge
-        assertTrue(null != dCE.getChild("role"));
+        assertNotNull(dCE.getChild("role"));
         assertEquals("org.codehaus.plexus.ISampleRole", dCE.getChildText("role"));
-        assertTrue(null != dCE.getChild("role-hint"));
+        assertNotNull(dCE.getChild("role-hint"));
         assertEquals("recessive-hint", dCE.getChildText("role-hint"));
-        assertTrue(null != dCE.getChild("lifecycle-handler"));
+        assertNotNull(dCE.getChild("lifecycle-handler"));
         assertEquals("plexus-configurable", dCE.getChildText("lifecycle-handler"));
-        assertTrue(null != dCE.getChild("requirements"));
+        assertNotNull(dCE.getChild("requirements"));
         assertEquals(1, dCE.getChild("requirements").getChildren("requirement").size());
         assertEquals(
                 "recessive-required-role-hint",
-                ((Element) dCE.getChild("requirements")
-                                .getChildren("requirement")
-                                .get(0))
-                        .getChildText("role-hint"));
+                dCE.getChild("requirements").getChildren("requirement").get(0).getChildText("role-hint"));
     }
 
-    public void testMergeRoleComponents() throws Exception {
+    @Test
+    void testMergeRoleComponents() throws Exception {
         SAXBuilder saxBuilder = new SAXBuilder();
 
         String source1 = "<component-set>\n" + "  <components>\n"
